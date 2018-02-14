ac9b3b90b45f463406b0d3b9b01f78e086c7934a
Rahul Banerjee <rb33547@gmail.com>
2018-02-14 14:47:12 +0530

    Add files via upload

---
+++ ducky_script_hello_world.ino
@@ -0,0 +1,53 @@
+#include <Keyboard.h>
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(10);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+
+  int i,n=5notepad
+  Hello World!!!
+  0;
+  // Begining the Keyboard stream
+  for(i=0;i<n;i++)
+  {
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(50);
+
+  delay(30);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(50);
+
+  Keyboard.print("notepad");
+
+  delay(30);
+
+  typeKey(KEY_RETURN);
+
+  delay(30);
+
+  Keyboard.print("Hello World!!!");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+  }
+}
+
+/* Unused endless loop */
+void loop() {}
---
+++ new_ducky_script.ino
@@ -0,0 +1,112 @@
+#include <Keyboard.h>
+
+
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  delay(3000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(500);
+
+  Keyboard.print("notepad");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.print("Hello bitch!!!");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+
+  //Entry 2
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(500);
+
+  Keyboard.print("photoshop");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  typeKey(KEY_RETURN);
+
+  //entry 3
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(500);
+
+  Keyboard.print("wordpad");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.print("You are an idiot!!!");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+
+  //entry 4
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(500);
+
+  Keyboard.print("cmd");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.print("diskpart");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+
+  // Ending stream
+  Keyboard.end();
+}
+
+void loop() {}
